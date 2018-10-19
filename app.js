32feccd03f130af2cd4e6382492d9655aef56fec
Evan Meshberg <evanmeshberg.student@careerdevs.com>
2018-10-19 00:01:30 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+/*
+app.get('/', function(request, response){
+   response.sendFile(__dirname, + '/public/index.html'); 
+});
+*/
+
+app.use(express.static('public'));
+
+app.listen(process.env.PORT);
